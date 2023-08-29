ac3646143d1ae8cf0bd42afedfc9742db6580997
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-08-29 19:53:16 -0400

    connection to mongodb atlas
    now the app can connect to mongodb atlas in the cloud

--- src/main/java/com/bicheka/BichekaApplication.java
+++ src/main/java/com/bicheka/BichekaApplication.java
@@ -3,6 +3,7 @@ package com.bicheka;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.bicheka.s3.S3Service;
 
+// @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @SpringBootApplication
 @EnableMongoRepositories
 @EnableMongoAuditing
@@ -18,6 +20,7 @@ public class BichekaApplication {
 	@Autowired
 	S3Service s3Service;
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BichekaApplication.class, args);
 	}
